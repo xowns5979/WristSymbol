1f52935a336c91d9d8670f67cefa526ab61f2f1b
taejun kim <xowns5979@users.noreply.github.com>
2020-02-28 10:49:34 +0900

    update

--- analyze/resultArrangement.R
+++ analyze/resultArrangement.R
@@ -2,14 +2,14 @@ library(dplyr)
 
 
 # Names
-names = c("p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12")
-cond = c("Baseline1","Approach","Baseline2")
+names = c("p1","p2","p3","p4","p5","p6","p7","p8","p9","p10")
+cond = c("Distal","Body")
 mode = c("training","main")
 # 1. 1 Letter Accuracy [%]  
 
 base_df = data.frame()
-for (i in 1:12){
-  file_name = paste("Exp6_data_diffCount_final/",names[i] ,"_",cond[3],"_",mode[2],".csv",sep="")
+for (i in 1:10){
+  file_name = paste("data/",names[i] ,"_",cond[2],"_",mode[2],".csv",sep="")
   file_data = read.csv(file_name, header=T, stringsAsFactors = F)
   base_df = rbind(base_df,file_data)
 }
@@ -21,11 +21,7 @@ result = group_by(base_df, id) %>%
   summarise(
     count = n(),
     correct = mean(correct)*100,
-    diff_1 = sum(diffCount_1),
-    diff_2 = sum(diffCount_2),
-    diff_3 = sum(diffCount_3)
-    #rt = mean(reaction_time)
-    #sd = sd(correct, na.rm = TRUE)*100
+    rt = mean(enterstamp - playendstamp)
   )
 print(result,n=36)
 sd(result$correct)
