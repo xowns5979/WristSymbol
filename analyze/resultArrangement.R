f596fe352ec682bdf8a0e2da17a56cc8cce094ac
taejun kim <xowns5979@users.noreply.github.com>
2020-03-05 21:00:22 +0900

    update

--- analyze/resultArrangement.R
+++ analyze/resultArrangement.R
@@ -2,14 +2,14 @@ library(dplyr)
 
 
 # Names
-names = c("p2","p3","p4","p6","p7","p8","p9","p10","p11","p12")
-cond = c("Distal","Body")
+names = c("p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12")
+cond = c("A(Baseline1)","B(Approach)","C(Baseline2)")
 mode = c("training","main")
 # 1. 1 Letter Accuracy [%]  
 
 base_df = data.frame()
-for (i in 1:10){
-  file_name = paste("data/",names[i] ,"_",cond[2],"_",mode[2],".csv",sep="")
+for (i in 1:12){
+  file_name = paste("data/",names[i] ,"_",cond[1],"_",mode[2],".csv",sep="")
   file_data = read.csv(file_name, header=T, stringsAsFactors = F)
   base_df = rbind(base_df,file_data)
 }
@@ -21,8 +21,9 @@ result = group_by(base_df, id) %>%
   summarise(
     count = n(),
     correct = mean(correct)*100,
-    rt = mean(enterstamp - playendstamp)
+    rt = mean(reactionTime)
   )
 print(result,n=36)
-sd(result$correct)
+write.csv(result,"result.csv")
+
 
