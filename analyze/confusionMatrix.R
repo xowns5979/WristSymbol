d980913970dd9e382d069f861e7ffa6455425de8
taejun kim <xowns5979@users.noreply.github.com>
2020-05-05 11:20:31 +0900

    update

--- analyze/confusionMatrix.R
+++ analyze/confusionMatrix.R
@@ -7,19 +7,128 @@ library(dplyr)
 
 
 # Names
-names = c("p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12")
-cond = c("A(Baseline1)","B(Approach)","C(Baseline2)")
-#cond = c("Distal","Body")
+names = c("p2","p3","p4","p6","p7","p8","p9","p10","p11","p12")
+cond = c("Distal","Body")
 mode = c("training","main")
 # 1. 1 Letter Accuracy [%]
 base_df = data.frame()
-for (i in 1:1){
-  file_name = paste("data/",names[i],"_",cond[1],"_",mode[2],".csv",sep="")
-  file_data = read.csv(file_name, header=T, stringsAsFactors = F)
-  base_df = rbind(base_df,file_data)
+for(j in 1:2)
+{
+  for (i in 1:10){
+    file_name = paste("Exp7_data/",names[i],"_",cond[j],"_",mode[2],".csv",sep="")
+    file_data = read.csv(file_name, header=T, stringsAsFactors = F)
+    base_df = rbind(base_df,file_data)
+  }  
 }
 
+base_df
+base_df_armfront = base_df[which(base_df$cond == "Distal"),]
+
+base_df_armbody = base_df[which(base_df$cond == "Body"),]
+
+
+base_df_armfront
+base_df_armbody
+
+
+x1 = ""
+x2 = ""
+
+for(i in 1:length(base_df_armbody$realPattern)){
+  if(substr(toString(base_df_armbody$realPattern[i]), 1,1) == "1"){
+    x1 = "3"
+  }
+  else if (substr(toString(base_df_armbody$realPattern[i]), 1,1) == "2"){
+    x1 = "1"
+  }
+  else if (substr(toString(base_df_armbody$realPattern[i]), 1,1) == "3"){
+    x1 = "4"
+  }
+  else if (substr(toString(base_df_armbody$realPattern[i]), 1,1) == "4"){
+    x1 = "2"
+  }
+  
+  if(substr(toString(base_df_armbody$realPattern[i]), 2,2) == "1"){
+    x2 = "3"
+  }
+  else if (substr(toString(base_df_armbody$realPattern[i]), 2,2) == "2"){
+    x2 = "1"
+  }
+  else if (substr(toString(base_df_armbody$realPattern[i]), 2,2) == "3"){
+    x2 = "4"
+  }
+  else if (substr(toString(base_df_armbody$realPattern[i]), 2,2) == "4"){
+    x2 = "2"
+  }
+  
+  modified = strtoi(paste(x1,x2,sep=""))
+  base_df_armbody$realPattern[i] = modified
+}
+
+
+
+
+
+
+x1 = ""
+x2 = ""
+
+for(i in 1:length(base_df_armbody$userAnswer)){
+  if(substr(toString(base_df_armbody$userAnswer[i]), 1,1) == "1"){
+    x1 = "3"
+  }
+  else if (substr(toString(base_df_armbody$userAnswer[i]), 1,1) == "2"){
+    x1 = "1"
+  }
+  else if (substr(toString(base_df_armbody$userAnswer[i]), 1,1) == "3"){
+    x1 = "4"
+  }
+  else if (substr(toString(base_df_armbody$userAnswer[i]), 1,1) == "4"){
+    x1 = "2"
+  }
+  
+  if(substr(toString(base_df_armbody$userAnswer[i]), 2,2) == "1"){
+    x2 = "3"
+  }
+  else if (substr(toString(base_df_armbody$userAnswer[i]), 2,2) == "2"){
+    x2 = "1"
+  }
+  else if (substr(toString(base_df_armbody$userAnswer[i]), 2,2) == "3"){
+    x2 = "4"
+  }
+  else if (substr(toString(base_df_armbody$userAnswer[i]), 2,2) == "4"){
+    x2 = "2"
+  }
+  
+  modified = strtoi(paste(x1,x2,sep=""))
+  base_df_armbody$userAnswer[i] = modified
+}
+
+base_df_armbody
+
+base_df = c()
+base_df = rbind(base_df, base_df_armfront)
+
+base_df
+
+
+realPattern_first = as.integer(substr(as.character(base_df$realPattern),2,2))
+userAnswer_first = as.integer(substr(as.character(base_df$userAnswer),2,2))
+#realPattern_first = as.integer(substr(as.character(base_df$realPattern),3,3))
+#userAnswer_first = as.integer(substr(as.character(base_df$userAnswer),3,3))
+
+realPattern_one = c()
+userAnswer_one = c()
+
+realPattern_one = c(realPattern_one, realPattern_first)
+userAnswer_one = c(userAnswer_one, userAnswer_first)
+
+str(realPattern_one)
+str(userAnswer_one)
+
 # Confusion matrix
+#actual_first <- as.factor(realPattern_one)
+#predicted_first <- as.factor(userAnswer_one)
 actual_first <- as.factor(base_df$realPattern)
 predicted_first <- as.factor(base_df$userAnswer)
 
@@ -28,7 +137,6 @@ str(predicted_first)
 
 actual_first
 predicted_first
-
 
 
 cm_first <- confusionMatrix(predicted_first, actual_first)
