b45f08abb6e3f1c630eb8e2c75f246e8b59fb0c2
taejun kim <xowns5979@users.noreply.github.com>
2020-07-25 17:51:22 +0900

    update

--- analyze/confusionMatrix.R
+++ analyze/confusionMatrix.R
@@ -7,16 +7,15 @@ library(dplyr)
 
 
 # Names
-names = c("p2","p3","p4","p6","p7","p8","p9","p10","p11","p12")
-cond = c("Distal","Body")
-#cond = c("Distal","Body")
+names = c("p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12")
+cond = c("armBody","armFront")
 mode = c("training","main")
 
 
 # 1. 1 Letter Accuracy [%]  
 base_df = data.frame()
 for (i in 1:10){
-  file_name = paste("Exp7_data/",names[i],"_",cond[2],"_",mode[2],".csv",sep="")
+  file_name = paste("Exp14_data/",names[i],"_",cond[2],"_",mode[2],".csv",sep="")
   file_data = read.csv(file_name, header=T, stringsAsFactors = F)
   base_df = rbind(base_df,file_data)
 }
@@ -27,14 +26,27 @@ base_df
 actual_first <- as.factor(base_df$realPattern)
 predicted_first <- as.factor(base_df$userAnswer)
 
-str(actual_first)
-str(predicted_first)
+actual_first_1 = strtoi(substr(actual_first,1,1))
+actual_first_2 = strtoi(substr(actual_first,2,2))
+actual_first_3 = strtoi(substr(actual_first,3,3))
 
-length(actual_first)
-length(predicted_first)
+predicted_first_1 = strtoi(substr(predicted_first,1,1))
+predicted_first_2 = strtoi(substr(predicted_first,2,2))
+predicted_first_3 = strtoi(substr(predicted_first,3,3))
+
+actual_first_real = as.factor(c(actual_first_1, actual_first_2, actual_first_3))
+predicted_first_real = as.factor(c(predicted_first_1, predicted_first_2, predicted_first_3))
 
 
-cm_first <- confusionMatrix(predicted_first, actual_first)
+
+str(actual_first_real)
+str(predicted_first_real)
+
+length(actual_first_real)
+length(predicted_first_real)
+
+
+cm_first <- confusionMatrix(predicted_first_real, actual_first_real)
 cm_temp = cm_first$table 
 
 cm_df <- as.data.frame(as.matrix(cm_temp))
