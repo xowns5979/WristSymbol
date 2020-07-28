f628a8a4ddf7cb052131c19e2873732b5fb11e39
taejun kim <xowns5979@users.noreply.github.com>
2020-07-28 17:05:11 +0900

    update

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -8,13 +8,15 @@ const int erm2 = 5; // 2
 const int erm3 = 6; // 3
 const int erm4 = 9; // 4
 
-bool stringComplete = false;
-char inData[200];
-int dataIdx = 0;
-
 bool ermOn[4] = {false, false, false, false};
+bool ermBurst[4] = {false, false, false, false};
+unsigned long lraBurstStartTime[4] = {0, 0, 0, 0};
+
+int ermBumpRunMS[4] = {100, 100, 100, 100}; // bump 진동 시간(ms)
+int ermBumpStopMS[4] = {0, 0, 0, 0};    // bump 쉬는 시간(ms)
+unsigned long ermRunStartTime[4] = {0, 0, 0, 0};  // erm 틀기 시작한 시간
+
 int intensity[4] = {255, 255, 255, 255};
-bool ermburst = false;
 
 
 //for motor test
@@ -30,17 +32,6 @@ void setup() {
   pinMode (erm4, OUTPUT);
 
   
-  analogWrite(erm1, 0);
-  analogWrite(erm2, 0);
-  analogWrite(erm3, 0);
-  analogWrite(erm4, 0);
-
-  //servo1.write(85);
-  //servo2.write(5);
-
-  // put your setup code here, to run once:
-  //servo1.attach(servo1Pin);
-  //servo2.attach(servo2Pin);
   Serial.begin(115200);
   while (! Serial);
   
@@ -50,6 +41,8 @@ void setup() {
 void loop() {
   loopSerial();
   loopMotorOnOff();
+
+  /*
   currenttime = millis();
   if(currenttime - recordedtime >= 500)
   {
@@ -59,6 +52,8 @@ void loop() {
       ermburst = false;  
     }
   }
+  */
+  
 }
 
 void loopSerial()
@@ -72,56 +67,172 @@ void loopSerial()
     char c3 = inString.charAt(2);
     char c4 = inString.charAt(3);
     char c5 = inString.charAt(4);
+    char c6 = inString.charAt(5);
 
-    int motorNum = 0;
-    switch(c1)
+    if(c2 == 'v')
     {
-      case 'e': // ERM motor
-        if(c2 == 'v')  // ERM motor vibrating
-        {
-          motorNum = (int)c3 - 49;
-          if(0 <= motorNum && motorNum < 4)
-            ermOn[motorNum] = true;
-        }
-        else if(c2 == 's')  // ERM motor stop
-        {
-          motorNum = (int)c3 - 49;
-          
-          if(0 <= motorNum && motorNum < 4)
-            ermOn[motorNum] = false;
-        }
-        else if(c2 == 't')
-        {
-          motorNum = (int)c3 - 49;
-          if(0 <= motorNum && motorNum < 4)
-          {
-            recordedtime = millis();
-            currentMotorNum = motorNum;
-            ermOn[motorNum] = true;
-            ermburst = true;
-          }
-        }
-        break;
-      case 'i':
-        int tactor = (int)c2-49;
-        intensity[tactor] = ((int)c3 - 48)*100 + ((int)c4 - 48)*10 + (int)c5 - 48;
-        break;
-      case 'z':
-        turnOffAll();
-        break;
-      default:
-        break;
-      
+      int motorNum = (int)c1 - 49;
+      if(0 <= motorNum && motorNum < 4)
+      {
+        ermOn[motorNum] = true;
+        ermRunStartTime[motorNum] = millis();
+            
+      }  
     }
+    else if (c2 == 's')
+    {
+      int motorNum = (int)c1 - 49;
+      if(0 <= motorNum && motorNum < 4)
+      {
+        ermOn[motorNum] = false;
+        ermTurnOff(motorNum);
+      }  
+    }
+    else if (c2 == 't')
+    {
+      int motorNum = (int)c1 - 49;
+      if(0 <= motorNum && motorNum < 4)
+      {
+        ermOn[motorNum] = true;
+        ermBurst[motorNum] = true;
+        ermRunStartTime[motorNum] = millis();  
+
+
+      }
+    }
+    else if (c2 == 'b' && c3 == 'v')
+    {
+      int motorNum = (int)c1 - 49;
+      int runMS = ((int)c4-48)*100+((int)c5-48)*10+((int)c6-48);
+      if(0 <= motorNum && motorNum < 4)
+      {
+        ermBumpRunMS[motorNum] = runMS;
+        
+        Serial.print("ERM motor ");
+        Serial.print(motorNum+1);
+        Serial.print(" BumpRunMS: ");
+        Serial.println(runMS);
+        Serial.flush();
+        
+      }
+    }
+    else if (c2 == 'b' && c3 == 's')
+    {
+      int motorNum = (int)c1 - 49;
+      int stopMS = ((int)c4-48)*100+((int)c5-48)*10+((int)c6-48);
+      if(0 <= motorNum && motorNum < 4)
+      {
+        ermBumpStopMS[motorNum] = stopMS;
+        
+        Serial.print("ERM motor ");
+        Serial.print(motorNum+1);
+        Serial.print(" BumpStopMS: ");
+        Serial.println(stopMS);
+        Serial.flush();
+        
+      }
+    }
+    
   }
 }
-
 
 // Function: loopMotorOnOff
 // Turn on LRA if true (166Hz full-powered)
 void loopMotorOnOff ()
 {
-  ermOnOff();
+  int i;
+  for(i=0;i<4;i++)
+  {
+    if(ermBurst[i])
+    {
+      unsigned long passedTime = millis() - ermRunStartTime[i];
+
+      /*
+      Serial.print("millis():  ");
+      Serial.print(millis());
+      Serial.print(", ermRunStartTime[i]: ");
+      Serial.print(ermRunStartTime[i]);
+      Serial.print(", passedTime: ");
+      Serial.println(passedTime);
+      */
+      if(passedTime > 500)
+      {       
+        ermTurnOff(i);
+        ermOn[i] = false;
+        ermBurst[i] = false;
+      }  
+    }
+    if(ermOn[i])
+    {
+      unsigned long overDrivingTime = 10;
+      
+      unsigned long passedTime = ((millis() - ermRunStartTime[i]) %(ermBumpRunMS[i]+ ermBumpStopMS[i]));
+      Serial.print("passedTime: ");
+      Serial.print(passedTime);
+
+
+       if(0 <= passedTime && passedTime < overDrivingTime)
+        {
+          ermTurnOn(i, 255);
+          Serial.println(", OverDrivingTime ");
+        }
+        else if(overDrivingTime <= passedTime && passedTime < ermBumpRunMS[i])
+        {
+          ermTurnOn(i, 100);  
+          Serial.println(", On Time ");
+        }
+        else
+        {
+          ermTurnOff(i);
+          Serial.println(", Off Time ");
+        }
+
+        /*
+      if(i == 2)
+      {
+        if(0 <= passedTime && passedTime < overDrivingTime)
+        {
+          ermTurnOn(i, 255);
+          Serial.println(", OverDrivingTime ");
+        }
+        else if(overDrivingTime <= passedTime && passedTime < ermBumpRunMS[i])
+        {
+          ermTurnOn(i, 200);  
+          Serial.println(", On Time ");
+        }
+        else
+        {
+          ermTurnOff(i);
+          Serial.println(", Off Time ");
+        }
+      }
+      else if (i == 1)
+      {
+        if(0 <= passedTime && passedTime < ermBumpRunMS[i])
+        {
+          ermTurnOn(i, 200);  
+          Serial.println(", On Time ");
+        }
+        else
+        {
+          ermTurnOff(i);
+          Serial.println(", Off Time ");
+        }
+      }
+      */
+      
+      /*
+      else
+      {
+        lraStop(i);
+        Serial.println(" - BumpStop");    
+      }
+      */
+        
+    }  
+  }
+
+  
 }
 
 void turnOffAll()
@@ -130,6 +241,32 @@ void turnOffAll()
   for(i=0;i<4;i++)
     ermOn[i] = false;
 }
+
+void ermTurnOn(int motorNum, int intensity)
+{
+  if(motorNum == 0)
+    analogWrite(erm1, intensity);
+  else if (motorNum == 1)
+    analogWrite(erm2, intensity);
+  else if (motorNum == 2)
+    analogWrite(erm3, intensity);
+  else if (motorNum == 3)
+    analogWrite(erm4, intensity);
+}
+
+void ermTurnOff(int motorNum)
+{
+  if(motorNum == 0)
+    analogWrite(erm1, 0);
+  else if (motorNum == 1)
+    analogWrite(erm2, 0);
+  else if (motorNum == 2)
+    analogWrite(erm3, 0);
+  else if (motorNum == 3)
+    analogWrite(erm4, 0);
+}
+
+
 
 void ermOnOff()
 {
